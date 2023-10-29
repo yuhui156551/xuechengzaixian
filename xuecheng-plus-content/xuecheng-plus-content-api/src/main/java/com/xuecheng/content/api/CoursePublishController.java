4f88a71015ab22e4c39fa34d0a8f0278ee68fbc5
yuhui <536430462@qq.com>
2023-10-29 15:04:34 +0800

    课程发布

--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CoursePublishController.java
@@ -2,10 +2,13 @@ package com.xuecheng.content.api;
 
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 import com.xuecheng.content.service.CoursePublishService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -29,6 +32,14 @@ public class CoursePublishController {
         modelAndView.addObject("model", coursePreviewInfo);
         modelAndView.setViewName("course_template");
         return modelAndView;
+    }
+
+    @ApiOperation("课程发布")
+    @ResponseBody
+    @PostMapping("/coursepublish/{courseId}")
+    public void coursepublish(@PathVariable("courseId") Long courseId) {
+        Long companyId = 1232141425L;
+        coursePublishService.publish(companyId,courseId);
     }
 
 }
--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/CoursePublishService.java
@@ -20,5 +20,13 @@ public interface CoursePublishService {
      */
     public CoursePreviewDto getCoursePreviewInfo(Long courseId);
 
-
+    /**
+     * @description 课程发布接口
+     * @param companyId 机构id
+     * @param courseId 课程id
+     * @return void
+     * @author Mr.M
+     * @date 2022/9/20 16:23
+     */
+    public void publish(Long companyId,Long courseId);
 }
